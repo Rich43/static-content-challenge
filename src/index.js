c7a5424dc7aaa5478683ee4688a8b32bd042a45a
Richard Ward <RichieS@GMail.com>
2022-09-21 20:09:51 +0100

    Render the blog page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+const marked = require("marked");
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  const defaultPage = 'blog/june/company-update';
+  const regex = /{{\s*(\w*)\s*}}/g;
+  let template = fs.readFileSync('./template.html', 'utf8');
+  const matches = template.matchAll(regex);
+  for (const match of matches) {
+    switch (match[1].toLowerCase()) {
+        case 'content':
+            let content = fs.readFileSync(
+                `./content/${defaultPage}/index.md`,
+                'utf8'
+            );
+
+            content = DOMPurify.sanitize(marked.parse(content));
+            template = template.replaceAll(match[0], content);
+            break;
+    }
+  }
+  res.send(template);
 });
 
 app.listen(3000, () => {
-  console.log("App listening on http://localhost:3000");
+  console.log('App listening on http://localhost:3000');
 });
